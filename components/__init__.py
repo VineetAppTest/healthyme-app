183b34f0ee9e15309b454933fd866223423c3c1d
VineetAppTest <vineetapptest@gmail.com>
2026-07-29 03:36:12 +0530

    Complete Member Home reminder email bootstrap

--- components/__init__.py
+++ components/__init__.py
@@ -2,7 +2,11 @@
 
 from components.member_email_bootstrap import install_member_email_notifications
 from components.member_email_followups import install_member_email_followups
+from components.member_email_legacy_reminders import (
+    install_legacy_schedule_reminder_delivery,
+)
 
 
 install_member_email_notifications()
 install_member_email_followups()
+install_legacy_schedule_reminder_delivery()
