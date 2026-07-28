2102324bbe8f2368d41b6978e939a9f397e6d1da
VineetAppTest <vineetapptest@gmail.com>
2026-07-29 03:32:58 +0530

    Install full member email notification coverage

--- components/__init__.py
+++ components/__init__.py
@@ -1,6 +1,8 @@
 """HealthyMe shared components package."""
 
 from components.member_email_bootstrap import install_member_email_notifications
+from components.member_email_followups import install_member_email_followups
 
 
 install_member_email_notifications()
+install_member_email_followups()
