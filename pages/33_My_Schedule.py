a30525e0cbafde0be851a4e392b9ccdd743a12cc
VineetAppTest <vineetapptest@gmail.com>
2026-07-29 14:29:40 +0530

    Apply successful-submit reset to Member reschedule form

--- pages/33_My_Schedule.py
+++ pages/33_My_Schedule.py
@@ -2,6 +2,9 @@ from components import guards, ui_common
 from components.package_hardening_schedule_ui import (
     install_package_hardening_schedule_ui,
 )
+from components.sprint1_schedule_hygiene import (
+    install_sprint1_schedule_hygiene,
+)
 import components.schedule_timezone_ui as schedule_timezone_ui
 
 
@@ -15,4 +18,5 @@ schedule_timezone_ui.topbar = ui_common.topbar
 schedule_timezone_ui.render_page_nav = ui_common.render_page_nav
 
 install_package_hardening_schedule_ui(schedule_timezone_ui, admin_page=False)
+install_sprint1_schedule_hygiene(schedule_timezone_ui)
 schedule_timezone_ui.render_member_schedule_page()
